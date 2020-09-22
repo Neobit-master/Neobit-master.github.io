0620bc9809df748b55c55cd7137eb9e1e72e6115
Abdiel Ortega <nathanabdiel1@gmail.com>
2020-09-22 15:54:34 -0500

    Lighthouse configured

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,6 +4,12 @@ module.exports = {
       startServerCommand: 'npm run start',
       url: ['http://localhost:8080']
     },
+    assert: {
+      assertions: {
+        'categories:performance': ['warn', { minScore: 0.8 }],
+        'categories:accessibility': ['error', { minScore: 0.8 }]
+      }
+    },
     upload: {
       target: 'temporary-public-storage',
     },
