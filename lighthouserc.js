e60e2b856dafb9c0c16170745b05b0f8902737d7
Abdiel Ortega <nathanabdiel1@gmail.com>
2020-09-22 16:30:35 -0500

    Continuous Integration was added

--- lighthouserc.js
+++ lighthouserc.js
@@ -6,7 +6,7 @@ module.exports = {
     },
     assert: {
       assertions: {
-        'categories:performance': ['warn', { minScore: 0.8 }],
+        'categories:seo': ['warn', { minScore: 0.8 }],
         'categories:accessibility': ['error', { minScore: 0.8 }]
       }
     },
