4ab82f34fe55576ed7b4a494e09b9462e0acc9ba
Abdiel Ortega <nathanabdiel1@gmail.com>
2021-02-02 16:40:47 -0600

    The login api was added

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 const Login = () => {
+  const formRef = useRef(null);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData(formRef.current);
+    console.log(formData.get('email'), formData.get('password'))
+    fetch('http://ec2-54-234-62-6.compute-1.amazonaws.com:8080/api/graphql', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: JSON.stringify({
+        query: `mutation {
+          login(username: "${formData.get('email')}", password: "${formData.get('password')}")
+        }`
+      })
+    })
+      .then(response => response.json())
+      .then(body => console.log(body));
+  }
   return (
     <div className="login">
-      <div className="login-container">
+      <form ref={formRef} className="login-container" onSubmit={handleSubmit}>
         <h2>Inicio de sesión</h2>
-        <form>
           <div className="input">
             <label htmlFor="email">Email:</label>
             <input type="email" name="email" id="email" placeholder="Email"/>
@@ -15,8 +34,7 @@ const Login = () => {
             <input type="password" name="password" id="password" placeholder="Contraseña"/>
           </div>
           <button className="button">Registrarse</button>
-        </form>
-      </div>
+      </form>
     </div>
   );
 };
